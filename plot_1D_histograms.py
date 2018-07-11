74777f446ba49aca3ff1eae0a41c058717376cc7
fkoehlin <fkoehlin@strw.leidenuniv.nl>
2018-07-11 14:55:12 +0900

    Improved plotting of 1D histograms. Should work better now for large parameter sets.

--- plot_1D_histograms.py
+++ plot_1D_histograms.py
@@ -8,56 +8,43 @@ Created on Fri Jun 29 16:45:45 2018
 
 import os
 #import numpy as np
+import math
 import matplotlib.pyplot as plt
 from plot_parameter_triangles import get_params_of_interest
 
 def plot_histogram(path_in, path_out, key_params=[], plot_filetypes=['.pdf'], hist_kwargs={}):
-    
+
     weights, points_cosmo, param_names, labels_chain = get_params_of_interest(path_in, key_params=key_params)
-    
-    #print weights.shape
-    #print points_cosmo.shape
-    
-    dim_x = len(param_names) / 2 
-    dim_y = 2 + len(param_names) % 2
-    
-    #print dim_x, dim_y, len(param_names)
-    #print param_names, len(param_names)
-    
-    fig, ax = plt.subplots(dim_y, dim_x, tight_layout=True)
-    
-    idx1 = 0
-    idx2 = 0
+
+    # Find the appropriate number of columns and lines for the 1d posterior
+    # plot
+    num_columns = int(round(math.sqrt(len(param_names))))
+    num_lines = int(math.ceil(len(param_names) * 1.0 / num_columns))
+
+    # my solution:
+    #num_columns = len(param_names) / 2
+    #num_lines = 2 + len(param_names) % 2
+
+    fig = plt.figure(figsize=(3 * num_columns, 3 * num_lines), tight_layout=True)
+
     for idx, param in enumerate(param_names):
-        
-        ax[idx1, idx2].set_title(r'$' + labels_chain[idx] + r'$')
-        ax[idx1, idx2].hist(points_cosmo[:, idx], weights=weights, **hist_kwargs) #'ls' = hist_kwargs['ls'], 'normed' = hist_kwargs['normed'])
-    
-        if idx2 == dim_x - 1:
-            idx2 = 0
-            idx1 += 1
-        else:
-            idx2 += 1
-        
-        #print idx1, idx2, idx
-    
-    # remove unused panels:    
-    if len(param_names) % 2 != 0: 
-        ax[-1, -1].axis('off')
-        ax[-1, -2].axis('off')
-        #print idx, idx1, idx2
-    
+
+        ax = fig.add_subplot(num_lines, num_columns, idx + 1, yticks=[])
+
+        ax.set_title(r'$' + labels_chain[idx] + r'$')
+        ax.hist(points_cosmo[:, idx], weights=weights, **hist_kwargs) #'ls' = hist_kwargs['ls'], 'normed' = hist_kwargs['normed'])
+
     for filetype in plot_filetypes:
         fname = os.path.join(path_out, 'histograms_1D' + filetype)
         fig.savefig(fname)
         print 'Plot saved to: \n', fname
-    
+
     return
 
 if __name__ == '__main__':
-    
+
     import sys
-    
+
     hist_kwargs = {'histtype': 'step',
                    'density': True,
                    'color': 'black',
@@ -66,16 +53,16 @@ if __name__ == '__main__':
                    'bins': 30
                   }
 
-    
+
     path_in = sys.argv[1]
-    
+
     # needs to be closed with '/' for glob.glob to work properly!
     if path_in[-1] != '/':
         path_in += '/'
 
     path_out = os.path.join(path_in, 'plots/')
-    
+
     if not os.path.isdir(path_out):
         os.makedirs(path_out)
-    
+
     plot_histogram(path_in, path_out, hist_kwargs=hist_kwargs)
